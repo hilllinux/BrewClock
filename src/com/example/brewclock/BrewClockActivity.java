c0d7c66f305d568cad6dd355757bd9fe0b5f1d49
wangsongqing <songqing1117@gmail.com>
2013-12-15 22:34:29 +0800

    change to a 12 min sport alarm

--- src/com/example/brewclock/BrewClockActivity.java
+++ src/com/example/brewclock/BrewClockActivity.java
@@ -1,25 +1,36 @@
 package com.example.brewclock;
 
 import android.app.Activity;
+import android.content.Context;
+import android.media.*;
+//import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.os.PowerManager;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
+import android.os.PowerManager.WakeLock;
 
 public class BrewClockActivity extends Activity implements OnClickListener {
   /** Properties **/
-  protected Button brewAddTime;
-  protected Button brewDecreaseTime;
   protected Button startBrew;
   protected TextView brewCountLabel;
   protected TextView brewTimeLabel;
-
+  PowerManager pm;
+  PowerManager.WakeLock mWakeLock; 
+  Uri notification;
+  Ringtone r;
+  
   protected int brewTime = 3;
   protected CountDownTimer brewCountDownTimer;
   protected int brewCount = 0;
   protected boolean isBrewing = false;
+  protected boolean is_break_time = false;
+  protected int stage_counts = 11;
+  protected int break_counts = 11;
   
   /** Called when the activity is first created. */
   @Override
@@ -28,20 +39,22 @@ public class BrewClockActivity extends Activity implements OnClickListener {
     setContentView(R.layout.main);
 
     // Connect interface elements to properties
-    brewAddTime = (Button) findViewById(R.id.brew_time_up);
-    brewDecreaseTime = (Button) findViewById(R.id.brew_time_down);
+    pm =  (PowerManager) getSystemService(Context.POWER_SERVICE);
+    mWakeLock = pm.newWakeLock(PowerManager.SCREEN_DIM_WAKE_LOCK, "BrewClock");
+    
+    notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+    r = RingtoneManager.getRingtone(getApplicationContext(), notification); 
+    
     startBrew = (Button) findViewById(R.id.brew_start);
     brewCountLabel = (TextView) findViewById(R.id.brew_count_label);
     brewTimeLabel = (TextView) findViewById(R.id.brew_time);
     
     // Setup ClickListeners
-    brewAddTime.setOnClickListener(this);
-    brewDecreaseTime.setOnClickListener(this);
     startBrew.setOnClickListener(this);
     
     // Set the initial brew values
     setBrewCount(0);
-    setBrewTime(3);
+    setBrewTime(60);
   }
   
   /** Methods **/
@@ -60,7 +73,7 @@ public class BrewClockActivity extends Activity implements OnClickListener {
     if(brewTime < 1)
       brewTime = 1;
 
-    brewTimeLabel.setText(String.valueOf(brewTime) + "m");
+    brewTimeLabel.setText(String.valueOf(brewTime));
   }
   
   /**
@@ -69,56 +82,70 @@ public class BrewClockActivity extends Activity implements OnClickListener {
    */
   public void setBrewCount(int count) {
     brewCount = count;
-    brewCountLabel.setText(String.valueOf(brewCount));
+    brewCountLabel.setText(String.valueOf(brewCount+1));
   }
   
   /**
    * Start the brew timer
    */
-  public void startBrew() {
-    // Create a new CountDownTimer to track the brew time
-    brewCountDownTimer = new CountDownTimer(brewTime * 60 * 1000, 1000) {
-      @Override
-      public void onTick(long millisUntilFinished) {
-        brewTimeLabel.setText(String.valueOf(millisUntilFinished / 1000) + "s");
-      }
-      
-      @Override
-      public void onFinish() {
-        isBrewing = false;
-        setBrewCount(brewCount + 1);
-        
-        brewTimeLabel.setText("Brew Up!");
-        startBrew.setText("Start");
-      }
-    };
-    
-    brewCountDownTimer.start();
-    startBrew.setText("Stop");
-    isBrewing = true;
-  }
+	public void startBrew() {
+		is_break_time = false;
+		stage_counts = 11;
+		break_counts = 11;
+		brewCount = 0;
+		brewCountDownTimer = new CountDownTimer(830 * 1000, 1000) {
+			long result;
+
+			public void onTick(long millisUntilFinished) {
+				if (millisUntilFinished < 1000 * (stage_counts * 60 + break_counts * 10)) {
+					if (is_break_time) {
+						stage_counts--;
+						is_break_time = false;
+					} else {
+						break_counts--;
+						is_break_time = true;
+					}
+					setBrewCount((brewCount + 1) % 4);
+					
+					r.play();
+				}
+
+				result = millisUntilFinished / 1000
+						- (stage_counts * 60 + break_counts * 10);
+				brewTimeLabel.setText(String.valueOf(result));
+			}
+
+			@Override
+			public void onFinish() {
+				startBrew.setText("Start");
+				brewCountDownTimer = null;
+			}
+		};
+
+		brewCountDownTimer.start();
+		isBrewing = true;
+		startBrew.setText("Stop");
+		mWakeLock.acquire();
+	}
   
   /**
    * Stop the brew timer
    */
   public void stopBrew() {
-    if(brewCountDownTimer != null)
+	isBrewing = false;
+    if(brewCountDownTimer != null){
       brewCountDownTimer.cancel();
-    
-    isBrewing = false;
+      brewCountDownTimer = null;
+    } else{
+    	brewTimeLabel.setText("Shit!");
+    }
     startBrew.setText("Start");
+    mWakeLock.release(); 
   }
   
   /** Interface Implementations **/
-  /* (non-Javadoc)
-   * @see android.view.View.OnClickListener#onClick(android.view.View)
-   */
   public void onClick(View v) {
-    if(v == brewAddTime)
-      setBrewTime(brewTime + 1);
-    else if(v == brewDecreaseTime)
-      setBrewTime(brewTime -1);
-    else if(v == startBrew) {
+    if(v == startBrew) {
       if(isBrewing)
         stopBrew();
       else
